ed35bf8ba8b651380ef1e17504337fbd17194076
oweenth <oweenth@gmail.com>
2017-10-26 00:15:18 +0300

    update(gulpfile.js):fix path to folders

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,12 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 
-gulp.task('sass', function () {
- return gulp.src('./public/stylesheets/**/*.scss')
-   .pipe(sass().on('error', sass.logError))
-   .pipe(gulp.dest('./css'));
+gulp.task('sass', function() {
+    return gulp.src('./public/stylesheets/scss/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./public/stylesheets/css'));
 });
 
-gulp.task('sass:watch', function () {
- gulp.watch('./sass/**/*.scss', ['sass']);
+gulp.task('sass:watch', function() {
+    gulp.watch('./public/stylesheets/scss/**/*.scss', ['sass']);
 });
